ed3c69ff23fd727d01597d271efdea39119b29e0
Hendrik Strobelt <hendrik@strobelt.com>
2014-02-27 23:00:41 -0500

    init

---
+++ Problem2/linegraph.js
@@ -0,0 +1,52 @@
+/**
+ * Created by hen on 2/20/14.
+ */
+    var bbVis, brush, createVis, dataSet, handle, height, margin, svg, svg2, width;
+
+    margin = {
+        top: 50,
+        right: 50,
+        bottom: 50,
+        left: 50
+    };
+
+    width = 960 - margin.left - margin.right;
+
+    height = 300 - margin.bottom - margin.top;
+
+    bbVis = {
+        x: 0 + 100,
+        y: 10,
+        w: width - 100,
+        h: 100
+    };
+
+    dataSet = [];
+
+    svg = d3.select("#vis").append("svg").attr({
+        width: width + margin.left + margin.right,
+        height: height + margin.top + margin.bottom
+    }).append("g").attr({
+            transform: "translate(" + margin.left + "," + margin.top + ")"
+        });
+
+
+    d3.csv("timeline.csv", function(data) {
+
+        // convert your csv data and add it to dataSet
+
+        return createVis();
+    });
+
+    createVis = function() {
+        var xAxis, xScale, yAxis,  yScale;
+
+          xScale = d3.scale.linear().domain([0,100]).range([0, bbVis.w]);  // define the right domain generically
+//        yScale = .. // define the right y domain and range -- use bbVis
+
+//        xAxis = ..
+//        yAxis = ..
+//        // add y axis to svg !
+
+
+    };
---
+++ Problem3/unHealth.js
@@ -0,0 +1,45 @@
+var bbDetail, bbOverview, dataSet, svg;
+
+var margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+};
+
+var width = 960 - margin.left - margin.right;
+
+var height = 800 - margin.bottom - margin.top;
+
+bbOverview = {
+    x: 0,
+    y: 10,
+    w: width,
+    h: 50
+};
+
+bbDetail = {
+    x: 0,
+    y: 100,
+    w: width,
+    h: 300
+};
+
+dataSet = [];
+
+svg = d3.select("#visUN").append("svg").attr({
+    width: width + margin.left + margin.right,
+    height: height + margin.top + margin.bottom
+}).append("g").attr({
+        transform: "translate(" + margin.left + "," + margin.top + ")"
+    });
+
+
+d3.csv("unHealth.csv", function(data) {
+
+});
+
+var convertToInt = function(s) {
+    return parseInt(s.replace(/,/g, ""), 10);
+};
+
