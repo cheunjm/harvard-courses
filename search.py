90b2377743aea89e120e827862abe2c98fa862fd
Jaemin Cheun <jcheun@college.harvard.edu>
2014-09-17 23:34:18 -0400

    Revert ab29045..8ed0a18
    This rolls back to commit ab29045bc17827e3d27ebebe433bc0d37452c2aa.

--- search.py
+++ search.py
@@ -65,12 +65,12 @@ def genericTreeSearch(problem, frontier, algorithm, heuristic = None):
         if not successor in visited_states:
           updated_states = successor, actions + (action,), visited_states + (current_state,)
           if algorithm == "ucs":
-            frontier.push(updated_states, problem.getCostOfActions(updated_states[1]))
-            print problem.getCostOfActions(actions)
+            frontier.push(updated_states, stepCost + problem.getCostOfActions(actions))
+            print(stepCost)
           elif algorithm == "Greedy":
             frontier.push(updated_states, heuristic(successor))
           elif algorithm == "aStar":
-            frontier.push(updated_states, problem.getCostOfActions(updated_states[1]) + heuristic(successor))
+            frontier.push(updated_states, stepCost + problem.getCostOfActions(actions) + heuristic(successor))
           else:
             frontier.push(updated_states)
   return []
